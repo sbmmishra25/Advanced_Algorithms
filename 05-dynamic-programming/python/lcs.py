9a89a7ff658d4c7b6d9affb18c2dd78e35bb07ec
Shubham Mishra <sbmmishra@gmail.com>
2026-09-21 16:16:49 +0530

    Add implementation 05-dynamic-programming/python/lcs.py

---
+++ 05-dynamic-programming/python/lcs.py
@@ -0,0 +1,13 @@
+def lcs(a, b):
+    n, m = len(a), len(b)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(n - 1, -1, -1):
+        for j in range(m - 1, -1, -1):
+            if a[i] == b[j]:
+                dp[i][j] = 1 + dp[i + 1][j + 1]
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+    return dp[0][0]
+
+if __name__ == "__main__":
+    print(lcs("AGGTAB", "GXTXAYB"))
