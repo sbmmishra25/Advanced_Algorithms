1f79ba6893d08e26dfb196c6fe23c97a79a8e648
Shubham Mishra <sbmmishra@gmail.com>
2026-09-21 16:17:10 +0530

    Add implementation 14-advanced-data-structures/python/dsu.py

---
+++ 14-advanced-data-structures/python/dsu.py
@@ -0,0 +1,25 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, a, b):
+        a, b = self.find(a), self.find(b)
+        if a == b:
+            return False
+        if self.size[a] < self.size[b]:
+            a, b = b, a
+        self.parent[b] = a
+        self.size[a] += self.size[b]
+        return True
+
+if __name__ == "__main__":
+    d = DSU(5)
+    d.union(0, 1); d.union(1, 2)
+    print(d.find(2) == d.find(0))
